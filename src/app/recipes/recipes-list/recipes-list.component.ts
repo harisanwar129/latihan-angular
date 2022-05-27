68eada7c25ff9b22b3a8e46df05c288e279955e0
harisanawar129 <harisanawar129@gmail.com>
2022-05-27 09:05:46 +0700

    pause project

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipes-list',
   templateUrl: './recipes-list.component.html',
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
-
+ recipes: Recipe[]=[
+  new Recipe('A test Recipe','this is simple test','https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
+  new Recipe('A test Recipe','this is simple test','https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg')
+ ];
   constructor() { }
 
   ngOnInit(): void {
